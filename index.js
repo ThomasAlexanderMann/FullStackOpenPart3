9366ea7631027367fb45ed8deae95d0343783bed
ThomasAlexanderMann <97682942+ThomasAlexanderMann@users.noreply.github.com>
2023-02-23 18:43:48 +0000

    Exercise 3.3 Phonebook backend step 3

--- index.js
+++ index.js
@@ -25,14 +25,25 @@ const phonebook = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
-});
-
 app.get("/info", (request, response) => {
   const line1 = `Phonebook has info for ${phonebook.length} people`;
   const line2 = new Date().toString();
   response.send(`<p>${line1}</p><p>${line2}</p>`);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(phonebook);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phonebook.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
